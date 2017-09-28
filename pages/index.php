31c179d74b6e787e109a5d305af81a824f37ee6b
tbaddade <thomas@redaxo.org>
2017-09-28 22:25:59 +0200

    Page include angepasst

--- pages/index.php
+++ pages/index.php
@@ -13,4 +13,5 @@ $subpage = rex_be_controller::getCurrentPagePart(2);
 
 echo rex_view::title(rex_i18n::msg('url_title'));
 
-include rex_be_controller::getCurrentPageObject()->getSubPath();
+//include rex_be_controller::getCurrentPageObject()->getSubPath();
+rex_be_controller::includeCurrentPageSubPath();
