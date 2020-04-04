6457df6bc19bb6fa6755537aeceec953627e20e3
Tobias Krais <tk@design-to-use.de>
2020-04-04 10:11:27 +0200

    finally solved

--- lib/Url/UrlManager.php
+++ lib/Url/UrlManager.php
@@ -281,14 +281,16 @@ class UrlManager
             return null;
         }
 
-        // Weiterleitung auf URL mit Suffix, wenn Suffix fehlt
-        $rewriterSuffix = Url::getRewriter()->getSuffix();
-        if (\rex::isFrontend() && $rewriterSuffix && substr($url->getPath(), -strlen($rewriterSuffix)) !== $rewriterSuffix && substr($url->getPath(), -4) !== ".xml") {
-            header('HTTP/1.1 301 Moved Permanently');
-            $url->handleRewriterSuffix();
+	// Weiterleitung auf URL mit Suffix, wenn Suffix fehlt
+	$rewriterSuffix = Url::getRewriter()->getSuffix();
+	if ($resolve && \rex::isFrontend() && $rewriterSuffix && substr($url->getPath(), -strlen($rewriterSuffix)) !== $rewriterSuffix) {
+		$url->handleRewriterSuffix();
+		if(count(UrlManagerSql::getByUrl(new Url($url->__toString()))) == 1) {
+			header('HTTP/1.1 301 Moved Permanently');
 			header('Location: '. $url->__toString());
-            exit;
-        }
+			exit;
+		}
+	}
 
         $items = UrlManagerSql::getByUrl($url);
         if (count($items) != 1) {
