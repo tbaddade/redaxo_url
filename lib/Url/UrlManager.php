33e139ca105492bc58896042a08c280158f3c4da
Tobias Krais <tk@design-to-use.de>
2020-04-23 23:24:14 +0200

    Zusätzliche Parameter bei Multidomainseiten (#187)
    Bei Multidomainseiten wurden zusätzliche Parameter nicht übergeben.

--- lib/Url/UrlManager.php
+++ lib/Url/UrlManager.php
@@ -401,7 +401,7 @@ class UrlManager
 
                 $scheme = Url::getRewriter()->getSchemeByDomain($url->getDomain()) ?: (Url::getRewriter()->isHttps() ? 'https' : 'http');
                 $url->withScheme($scheme);
-                return $url->getSchemeAndHttpHost().$url->getPath();
+                return $url->getSchemeAndHttpHost().$url->getPath().$url->getQuery();
             }
         }
         return null;
